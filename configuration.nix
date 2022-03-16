1997fa54242953f0782ae69f3680357effe34667
whitely <bengwhitely@gmail.com>
2022-03-15 23:24:39 -0700

    NetworkApplet to autojoin WiFi
    Our router changed to a more reliable one and starting nm-applet on i3 launch
      One of those two fixed wifi not autoconnecting, and I'm not sure which

--- configuration.nix
+++ configuration.nix
@@ -154,7 +154,12 @@
   ];
 
   # Fish!
-  programs.fish.enable = true;
+  programs = {
+    # Fish!
+    fish.enable = true;
+    # Network manager applet
+    nm-applet.enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
